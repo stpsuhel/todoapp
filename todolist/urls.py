6b39f8c7752b73533f2d1b8e26065cd305f3db77
Suhel Rana <suhelrana53@gmail.com>
2020-02-23 00:59:33 +0600

    delete from list

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,4 +4,7 @@ from  . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('delete/<list_id>', views.delete, name='delete'),
+    path('complete/<list_id>', views.complete, name='complete'),
+    path('incomplete/<list_id>', views.incomplete, name='incomplete'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
 
 
 def home(request):
@@ -8,6 +9,7 @@ def home(request):
         form = ListForm(request.POST or None)
         if form.is_valid():
             form.save()
+            messages.success(request, "Item has been added to List!")
 
     all_item = List.objects.all
     return render(request, 'home.html', {'all_item': all_item})
@@ -15,3 +17,24 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, "Item has been deleted from List!")
+    return redirect("home")
+
+
+def complete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect("home")
+
+
+def incomplete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect("home")
